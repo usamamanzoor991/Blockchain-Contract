ba0d5ab9126eb8d7807b1515fbfb8501826f715a
Usama Manzoor <usamamanzoor991@gmail.com>
2025-05-01 11:43:05 +0500

    UI Updated

--- src/Pages/Static/Home.jsx
+++ src/Pages/Static/Home.jsx
@@ -6,13 +6,17 @@ import { abi } from '../../utilities/ABI/abi.js';
 const Home = () => {
 
     const providerRef = useRef();
-    const [signer,setSigner] = useState();
     const contractRef = useRef();
     const [balanceB,setBalanceB] = useState();
     const [balanceA,setBalanceA] = useState();
+    const [signerAddress,setSignerAddress] = useState();
+    const [loading,setLoading] = useState(false);
+    
+    const contractAddress = "0xc365Fc4c14259b861Dddbe771618ab9258da32f3";
 
-    //Get Provider And Signer Function - Provider(Read Only) | Signer(Read/Write) 
-    const getProviderAndSigner = async() => {
+    //Get Essentials - Provider | Signer | Contract 
+    const getEssentials = async() => {
+        setLoading(true);
         if(window.ethereum == null){
             console.log("MetaMask not installed; using read-only defaults")
             const Provider = ethers.getDefaultProvider();
@@ -22,91 +26,99 @@ const Home = () => {
             const Provider = new ethers.BrowserProvider(window.ethereum);
             providerRef.current = Provider;
         }
+
         const Signer = await providerRef.current.getSigner();
-        setSigner(Signer);
+
+        await providerRef.current.send('eth_requestAccounts', []);
+        const contract = new Contract(contractAddress, abi, Signer);
+        contractRef.current = contract;
+
+        const address = await Signer.getAddress();
+        setSignerAddress(address); 
     }
 
-    //Get Contract Function
-    const getContract = async() => {
-        if(signer!= undefined){
-            await providerRef.current.send('eth_requestAccounts', []);
-            const contract = new Contract("0xc365Fc4c14259b861Dddbe771618ab9258da32f3", abi, signer);
-            contractRef.current = contract;
+    const getBalance = async() => {
+        if(signerAddress){
+            const balance = await providerRef.current.getBalance(signerAddress);
+            const formattedBalance = formatEther(balance);
+            setLoading(false);
+            return formattedBalance;
         }
     }
 
-    const mintTokens = async() => {
-        if(signer!= undefined){
-            const address = await signer.getAddress();
-            const balance = await providerRef.current.getBalance(address);
-            setBalanceB(formatEther(balance));
-        }
-    }
-
-    //Get Provider And Signer Call
+    //Get Essentials Call
     useEffect(()=>{
-        getProviderAndSigner();
-    },[])
-
-    //Get Contract Call
-    useEffect(()=>{
-        getContract();
-    },[signer])
-
-    useEffect(()=>{
-        mintTokens();
-    },[signer])
-
-    //Test
-    useEffect(()=>{
-        console.log("Provider : ",providerRef);
-        console.log("Signer : ",signer);
-        console.log("Contract : ",contractRef.current);
-        console.log("Before Balance : ",balanceB);
-    },[signer,balanceB])
+        getEssentials();
+        const balance = getBalance();
+        setBalanceB(balance);
+        setBalanceA(balance);
+    },[signerAddress])
 
     const handleMint = async() => {
+        setLoading(true);
         try {
             const tx = await contractRef.current.publicMint(10);
             await tx.wait();
             alert("Minted Successfully!!!");
+            setLoading(false);
         } catch (error) {
             console.error("Error Minting Tokens : ",error);
+            setLoading(false);
         }
     }
 
     const handleTransfer = async() => {
+        setLoading(true);
         try {
-            const tx = await contractRef.current.transfer("0xc365Fc4c14259b861Dddbe771618ab9258da32f3", 10);
+            const tx = await contractRef.current.transfer(contractAddress, 10);
             await tx.wait();
-            alert("Transfer successful!");
+            alert("Transfer successfull!");
+            setLoading(false);
         } catch (error) {
-            console.error("Error Minting Tokens : ",error);
+            console.error("Error Transferring Tokens : ",error);
+            setLoading(false);
         }
     }
 
     const handleApprove = async() => {
+        setLoading(true);
         try {
-            const tx = await contractRef.current.approve("0xc365Fc4c14259b861Dddbe771618ab9258da32f3", 10);
+            const tx = await contractRef.current.approve(contractAddress, 10);
             await tx.wait();
-            alert("Approved successful!");
-            const address = await signer.getAddress();
-            const balance = await providerRef.current.getBalance(address);
-            setBalanceA(formatEther(balance));
+            alert("Approved successfully!");
+            const balance = await getBalance(signerAddress);
+            setBalanceA(balance);
+            setLoading(false);
         } catch (error) {
-            console.error("Error Minting Tokens : ",error);
+            console.error("Error Approving Tokens : ",error);
+            setLoading(false);
         }
     }
 
-    return (
-        <div className='flex flex-col'>
-          <button className='bg-orange-100 mb-[100px]' onClick={() => handleMint()}>Mint Token</button>
-          <button className='bg-orange-100 mb-4' onClick={() => handleTransfer()}>Handle Transfer</button>
-          <button className='bg-orange-100 mb-4' onClick={() => handleApprove()}>Handle Approve</button>
-          <h1>Balance Before : {balanceB}</h1>
-          <h1>Balance After : {balanceA}</h1>
-        </div>
-      )
+    if(loading){
+        return(
+            <div className='min-h-[100vh] flex flex-column justify-center items-center'>
+                <h1 className='text-black'>loading...</h1>
+            </div>
+        )
+    }
+
+    if(!loading){
+        return (
+            <div className='bg-[#fefae0] min-h-[100vh]'>
+                <h1 className='text-center text-[32px] font-bold pt-4 pb-4'>SepoliaETH Testnet</h1>
+                <div className='flex flex-col justify-center items-center'>
+                    <button className='bg-[#bc6c25] text-[#fefae0] mb-4 p-4 rounded-[2.25rem] w-[90vw] md:w-[40vw] hover:cursor-pointer'  onClick={() => handleMint()}>Mint Token</button>
+                    <button className='bg-[#bc6c25] text-[#fefae0] mb-4 p-4 rounded-[2.25rem] w-[90vw] md:w-[40vw] hover:cursor-pointer' onClick={() => handleTransfer()}>Handle Transfer</button>
+                    <button className='bg-[#bc6c25] text-[#fefae0] mb-4 p-4 rounded-[2.25rem] w-[90vw] md:w-[40vw] hover:cursor-pointer' onClick={() => handleApprove()}>Handle Approve</button>
+                    <div className='flex flex-col justify-center items-center bg-[#606c38] w-[90vw] md:w-[40vw] rounded-[2.25rem] p-4'>
+                        <h2 className='mb-4 font-extrabold inline-block text-[#fefae0]'>Balance Before : <p className='font-normal inline-block'>{balanceB}</p></h2>
+                        <h2 className='font-extrabold inline-block text-[#fefae0]'>Balance After : <p className='font-normal inline-block'>{balanceA}</p></h2>
+                    </div>    
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Home;
